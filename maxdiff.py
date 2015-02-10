99dcb736422748f5c8c34c1f09563546a975dbc0
Sebastien Genty <paperparrot@me.com>
2015-02-09 17:25:18 -0800

    Update maxdiff.py
    updated var names, added fixed item

--- maxdiff.py
+++ maxdiff.py
@@ -11,7 +11,7 @@ the attributes tested. Input for now is .csv though also working on a .xlsx solu
 
 
 def avg_imp(utilities_file, filter_var=None, weight=None):
-    """ Actual function doing all the heavy lifting. Takes in the utility scores and filters and computes the average
+    """ Actual function doing all the heavy lifting. Takes in the utility scores and filter_df and computes the average
     importances. Has the option of adding weights if need be.
     :param utilities_file: CSV file containing the utility scores. Top row should be labels.
     :param filter_var: CSV file containing the filter values. Each filter group should be its own variable, no overlaps
@@ -20,32 +20,33 @@ def avg_imp(utilities_file, filter_var=None, weight=None):
     """
 
     # Load the file, format data
-    raw = pd.read_csv(utilities_file, index_col='Respondent')
-    raw = raw.drop('RLH', axis=1)
+    raw_df = pd.read_csv(utilities_file, index_col='Respondent')
+    raw_df = raw_df.drop('RLH', axis=1)
+    raw_df['fixed_item'] = 0
 
     # Exponentiate the utilities, rescale so the sum is 100 for each respondent
-    rescaled = np.exp(raw)
-    rescaled = (rescaled.divide(rescaled.sum(axis=1), axis=0)*100)
+    rescaled_df = np.exp(raw_df)
+    rescaled_df = (rescaled_df.divide(rescaled_df.sum(axis=1), axis=0)*100)
 
     # Taking care of the weighting scheme. Weights are loaded, applied and then discarded
     if weight is not None:
         weights = pd.read_csv(weight, index_col='session')
-        rescaled = pd.concat(rescaled, weights)
-        rescaled.apply(lambda x: np.asarray(x) * np.asarray(rescaled['weight']))
-        rescaled = rescaled.drop('weight')
+        rescaled_df = pd.concat(rescaled_df, weights)
+        rescaled_df.apply(lambda x: np.asarray(x) * np.asarray(rescaled_df['weight']))
+        rescaled_df = rescaled_df.drop('weight', axis=1)
 
     # If filters are needed, it adds them, and creates tables by each sub filter
     if filter_var is None:
-        output = rescaled.mean()
+        output = rescaled_df.mean()
         rescaled_output = output*100/(output.mean())
     else:
-        filters = pd.read_csv(filter_var, index_col='session')
-        data = pd.merge(rescaled, filters, left_index=True, right_index=True)
+        filter_df = pd.read_csv(filter_var, index_col='session')
+        data = pd.merge(rescaled_df, filter_df, left_index=True, right_index=True)
 
         # Loop that goes through each filter group and generates a row for each.
         # Those are then added to the output table.
         output = pd.DataFrame(data.mean()).transpose()
-        for column in filters.columns:
+        for column in filter_df.columns:
             local_df = data.groupby(column).mean()
             local_df['filter'] = column
             # add to running total
@@ -53,12 +54,12 @@ def avg_imp(utilities_file, filter_var=None, weight=None):
 
         # Filter columns are now deleted
 
-        for column in filters.columns:
+        for column in filter_df.columns:
             output = output.drop(column, axis=1)
 
-        filt_list = output['filter']
+        filt_list = pd.Series(output['filter'])
         output = output.drop('filter', axis=1)
-        rescaled_output = output.divide(output.mean(axis=1))*100
-        rescaled_output = pd.concat(rescaled_output, filt_list)
-
+        # rescaled_output = output.divide(output.mean(axis=1))*100
+        # rescaled_output = pd.concat(rescaled_output, filt_list)
+        rescaled_output = output
     return rescaled_output
